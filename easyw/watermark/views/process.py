528ef676aa6ee067469fa44f153b688b7125f49d
GooZy <ziyaoguo@gmail.com>
2018-05-25 19:35:48 +0800

    feat: DIY coefficient

--- easyw/watermark/views/process.py
+++ easyw/watermark/views/process.py
@@ -6,6 +6,8 @@
 Watermark processing
 """
 from easyw.watermark.biz.image import ImageBiz
+from config.default import SCRAMBLING_KEY
+from config.default import COEFFICIENTS
 
 from flask import Blueprint
 from flask import render_template
@@ -43,6 +45,16 @@ def dwt_index():
 def perform_dwt():
     cover_image = request.files.get('cover_image')
     watermark = request.files.get('watermark')
+    times = int(request.form.get('times'))
+    a = float(request.form.get('cA'))
+    b = float(request.form.get('cH'))
+    c = float(request.form.get('cV'))
+    d = float(request.form.get('cD'))
+    COEFFICIENTS[0] = a
+    COEFFICIENTS[1] = b
+    COEFFICIENTS[2] = c
+    COEFFICIENTS[3] = d
+    SCRAMBLING_KEY[0] = times
     if not cover_image or not watermark:
         return render_template('dwt.html', error='Upload files error!')
     # save files to local path
